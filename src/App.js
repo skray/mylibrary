46efbe0036e86eb1a857b08918f4323a39835352
Sean Ray <sean.ray@wwt.com>
2019-02-25 12:39:22 -0500

    Add Bootstrap, FontAwesome and update icon
    * Add react-bootstrap
    * Add react-fontawesome
    * Add nav and footer
    * Add link to Github repo
    * Update favicon and add icon to nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import _debounce from 'lodash/debounce';
+import booksLogo from "./media/books.png";
+
+import './App.scss';
 
 class App extends Component {
 
@@ -74,16 +79,44 @@ class App extends Component {
   
   render() {
     return (
-      <div>
-        <h1>MyLibrary</h1>
+      <div id="wrapper">
 
-        <input type="text" autoFocus onChange={this.handleSearchChange}/>
+        <nav className="header navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/">
+            <img src={booksLogo} className="d-inline-block align-middle mr-2" width="30" height="30" alt="" />
+              My Library
+            </a>
+          <div className="navbar-nav mr-auto">
+            
+          </div>
+
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a href="https://github.com/skray/mylibrary" target="_blank" rel="noopener noreferrer" className="nav-link">
+                <FontAwesomeIcon icon={faGithub} className="fa-lg"/>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div id="mainContent" className="pt-3 container-fluid">
         
-        <p>{this.state.message}</p>
-        
-        <ul>
-          {this.state.books.map(book => <li key={book.id}>{ book.best_book.title }</li>)}
-        </ul>
+          <input type="text" autoFocus onChange={this.handleSearchChange}/>
+          
+          <p>{this.state.message}</p>
+          
+          <ul>
+            {this.state.books.map(book => <li key={book.id}>{ book.best_book.title }</li>)}
+          </ul>
+        </div>
+        <div className="footer navbar navbar-expand-lg navbar-light bg-light" >
+          Books icon made by&nbsp;
+          <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>&nbsp; 
+          from&nbsp; 
+          <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>&nbsp;
+          is licensed by&nbsp; 
+          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" rel="noopener noreferrer" target="_blank">CC 3.0 BY</a>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
